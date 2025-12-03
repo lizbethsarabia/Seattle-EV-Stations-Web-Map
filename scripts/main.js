3a4960a8a346b16b878ba145091ec062f35485eb
branv <branvphung@gmail.com>
2025-12-02 21:42:28 -0800

    Re-added custom basemap, still not working

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiam9vZ2xlYmVycnkiLCJhIjoiY202YjZqcHp3MDVjeDJqb
 
 const map = new mapboxgl.Map({
     container: 'map',
+    //style: 'mapbox://styles/joogleberry/cmip84zdn002k01ssamty38p5',
     style: 'mapbox://styles/mapbox/streets-v8',
     center: [-122.335, 47.623],
     zoom: 10.5
